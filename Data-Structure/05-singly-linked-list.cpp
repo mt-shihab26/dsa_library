e56844ac40c928e0ba5b699f2a597f4fc590ec06
Shihab Mahamud <shihab4t@gmail.com>
2022-10-18 16:45:23 +0600

    Singly linked list with cpp

--- Data-Structure/05-singly-linked-list.cpp
+++ Data-Structure/05-singly-linked-list.cpp
@@ -1,48 +1,138 @@
+// Singly Linked List Implementation
+
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Node
+class Node
 {
+public:
     int value;
     Node *next;
-
-    Node(int _value)
+    Node(int value, Node *next)
     {
-        value = _value;
-        next = nullptr;
+        this->value = value;
+        this->next = next;
     }
 };
 
 class SinglyLinkedList
 {
-    Node *_head;
-    size_t _size;
-
-public:
-    SinglyLinkedList()
-    {
-        _head = nullptr;
-    }
-
-    ~SinglyLinkedList()
-    {
-    }
+    Node *head = nullptr;
 
     bool is_empty()
     {
-        return _head == nullptr;
+        return head == nullptr;
+    }
+
+public:
+    void push_back(int value)
+    {
+        Node *node = new Node(value, nullptr);
+
+        if (is_empty())
+            head = node;
+        else
+        {
+            Node *tmp_head = head;
+            while (tmp_head->next != nullptr)
+                tmp_head = tmp_head->next;
+            tmp_head->next = node;
+        }
     }
 
     void push_front(int value)
     {
-        Node *node = new Node(value);
-        if (is_empty() == false)
-            node->next = _head;
-        _head = node;
-        _size++;
+        auto node = new Node(value, nullptr);
+
+        if (is_empty())
+            head = node;
+        else
+        {
+            node->next = head;
+            head = node;
+        }
+    }
+
+    void push_at(int index, int value)
+    {
+        auto node = new Node(value, nullptr);
+        if (is_empty())
+            head = node;
+        else
+        {
+            Node *tmp_head = head;
+            for (int i = 1; i < index; i++)
+                tmp_head = tmp_head->next;
+            node->next = tmp_head->next;
+            tmp_head->next = node;
+        }
+    }
+
+    void print()
+    {
+        Node *tmp_node = head;
+        while (tmp_node != nullptr)
+        {
+            cout << tmp_node->value << " ";
+            tmp_node = tmp_node->next;
+        }
+        cout << "\n";
+    }
+
+    int get_at(int index)
+    {
+        Node *tmp_head = head;
+        for (int i = 1; i <= index; i++)
+            tmp_head = tmp_head->next;
+        return tmp_head->value;
+    }
+
+    void update(int index, int value)
+    {
+        Node *tmp_head = head;
+        for (int i = 1; i <= index; i++)
+            tmp_head = tmp_head->next;
+        tmp_head->value = value;
+    }
+
+    void remove(int index)
+    {
+        Node *tmp_head = head;
+        for (int i = 1; i < index; i++)
+            tmp_head = tmp_head->next;
+        Node *tmp_next = tmp_head->next->next;
+        delete tmp_head->next;
+        tmp_head->next = tmp_next;
+    }
+    ~SinglyLinkedList()
+    {
+        while (head != nullptr)
+        {
+            Node *tmp_next = head->next;
+            delete head;
+            head = tmp_next;
+        }
     }
 };
 
 int main()
 {
+    SinglyLinkedList l;
+
+    l.push_back(10);  // in     : 10
+    l.push_front(14); // in     : 14 10
+
+    l.push_back(17); // in      : 14 10 17
+    l.push_back(19); // in      : 14 10 17 19
+
+    l.push_at(2, 87); // in     : 14 10 87 17 19
+
+    l.print();                   // stdout      : 14 10 87 17 19
+    cout << l.get_at(3) << "\n"; // return      : 17
+
+    l.update(3, 77); // in          : 14 10 87 77 19
+    l.print();       // stdout      : 14 10 87 77 19
+
+    l.remove(2); // in          : 14 10 77 19
+    l.print();   // stdout      : 14 10 77 19
 }
