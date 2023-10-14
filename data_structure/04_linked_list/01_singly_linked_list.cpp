4b4e019467709409f1ba8094c0e5f383d014571a
Shihab Mahamud <shihab4t@gmail.com>
2023-10-14 09:26:27 +0600

    clang format included

--- data_structure/04_linked_list/01_singly_linked_list.cpp
+++ data_structure/04_linked_list/01_singly_linked_list.cpp
@@ -5,10 +5,10 @@ using namespace std;
 
 class Node
 {
-public:
+  public:
     int value;
     Node *next;
-    Node(int value, Node *next)
+    Node (int value, Node *next)
     {
         this->value = value;
         this->next = next;
@@ -19,67 +19,73 @@ class SinglyLinkedList
 {
     Node *head = nullptr;
 
-    bool is_empty()
+    bool
+    is_empty ()
     {
         return head == nullptr;
     }
 
-public:
-    void push_back(int value)
+  public:
+    void
+    push_back (int value)
     {
-        Node *node = new Node(value, nullptr);
+        Node *node = new Node (value, nullptr);
 
-        if (is_empty())
+        if (is_empty ())
             head = node;
         else
-        {
-            Node *tmp_head = head;
-            while (tmp_head->next != nullptr)
-                tmp_head = tmp_head->next;
-            tmp_head->next = node;
-        }
+            {
+                Node *tmp_head = head;
+                while (tmp_head->next != nullptr)
+                    tmp_head = tmp_head->next;
+                tmp_head->next = node;
+            }
     }
 
-    void push_front(int value)
+    void
+    push_front (int value)
     {
-        auto node = new Node(value, nullptr);
+        auto node = new Node (value, nullptr);
 
-        if (is_empty())
+        if (is_empty ())
             head = node;
         else
-        {
-            node->next = head;
-            head = node;
-        }
+            {
+                node->next = head;
+                head = node;
+            }
     }
 
-    void push_at(int index, int value)
+    void
+    push_at (int index, int value)
     {
-        auto node = new Node(value, nullptr);
-        if (is_empty())
+        auto node = new Node (value, nullptr);
+        if (is_empty ())
             head = node;
         else
-        {
-            Node *tmp_head = head;
-            for (int i = 1; i < index; i++)
-                tmp_head = tmp_head->next;
-            node->next = tmp_head->next;
-            tmp_head->next = node;
-        }
+            {
+                Node *tmp_head = head;
+                for (int i = 1; i < index; i++)
+                    tmp_head = tmp_head->next;
+                node->next = tmp_head->next;
+                tmp_head->next = node;
+            }
     }
 
-    void print()
+    void
+    print ()
     {
         Node *tmp_node = head;
         while (tmp_node != nullptr)
-        {
-            cout << tmp_node->value << " ";
-            tmp_node = tmp_node->next;
-        }
+            {
+                cout << tmp_node->value << " ";
+                tmp_node = tmp_node->next;
+            }
         cout << "\n";
     }
 
-    int get_at(int index)
+    int
+    get_at (int index)
     {
         Node *tmp_head = head;
         for (int i = 1; i <= index; i++)
@@ -87,7 +93,8 @@ public:
         return tmp_head->value;
     }
 
-    void update(int index, int value)
+    void
+    update (int index, int value)
     {
         Node *tmp_head = head;
         for (int i = 1; i <= index; i++)
@@ -95,7 +102,8 @@ public:
         tmp_head->value = value;
     }
 
-    void remove(int index)
+    void
+    remove (int index)
     {
         Node *tmp_head = head;
         for (int i = 1; i < index; i++)
@@ -105,7 +113,8 @@ public:
         tmp_head->next = tmp_next;
     }
 
-    void remove_back()
+    void
+    remove_back ()
     {
         Node *tmp_head = head;
         while (tmp_head->next->next != nullptr)
@@ -114,49 +123,51 @@ public:
         tmp_head->next = nullptr;
     }
 
-    void remote_front()
+    void
+    remote_front ()
     {
         Node *tmp_next = head->next;
         delete head;
         head = tmp_next;
     }
 
-    ~SinglyLinkedList()
+    ~SinglyLinkedList ()
     {
         while (head != nullptr)
-        {
-            Node *tmp_next = head->next;
-            delete head;
-            head = tmp_next;
-        }
+            {
+                Node *tmp_next = head->next;
+                delete head;
+                head = tmp_next;
+            }
     }
 };
 
-int main()
+int
+main ()
 {
     SinglyLinkedList l;
 
-    l.push_back(10);  // in     : 10
-    l.push_front(14); // in     : 14 10
+    l.push_back (10);  // in     : 10
+    l.push_front (14); // in     : 14 10
 
-    l.push_back(17); // in      : 14 10 17
-    l.push_back(19); // in      : 14 10 17 19
+    l.push_back (17); // in      : 14 10 17
+    l.push_back (19); // in      : 14 10 17 19
 
-    l.push_at(2, 87); // in     : 14 10 87 17 19
+    l.push_at (2, 87); // in     : 14 10 87 17 19
 
-    l.print();                   // stdout      : 14 10 87 17 19
-    cout << l.get_at(3) << "\n"; // return      : 17
+    l.print ();                   // stdout      : 14 10 87 17 19
+    cout << l.get_at (3) << "\n"; // return      : 17
 
-    l.update(3, 77); // in          : 14 10 87 77 19
-    l.print();       // stdout      : 14 10 87 77 19
+    l.update (3, 77); // in          : 14 10 87 77 19
+    l.print ();       // stdout      : 14 10 87 77 19
 
-    l.remove(2); // in          : 14 10 77 19
-    l.print();   // stdout      : 14 10 77 19
+    l.remove (2); // in          : 14 10 77 19
+    l.print ();   // stdout      : 14 10 77 19
 
-    l.remove_back();
-    l.remove_back();
-    l.print(); // stdout      : 14 10
+    l.remove_back ();
+    l.remove_back ();
+    l.print (); // stdout      : 14 10
 
-    l.remote_front();
-    l.print(); // stdout : 10
+    l.remote_front ();
+    l.print (); // stdout : 10
 }
