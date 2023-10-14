9226894e47c3c3ca076fbdf6770dbe62d2d9c7d3
Shihab Mahamud <shihab4t@gmail.com>
2023-10-14 09:49:59 +0600

    clang format included LLV

---
+++ data_structure/04_linked_list/02_singly_linked_list2.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node {
+    int value;
+    Node *next;
+
+  public:
+    Node(int value, Node *next) {
+        this->value = value;
+        this->next = next;
+    }
+};
+
+class SinglyLinkedList {};
+
+int main() {}
