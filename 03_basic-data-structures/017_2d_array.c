4721d84d4a02f5a5bc3511fc7c366ad3c05bf98c
Shihab Mahamud <shihab4t@gmail.com>
2025-06-30 21:05:13 +0600

    2d array

--- 03_basic-data-structures/017_2d_array.c
+++ 03_basic-data-structures/017_2d_array.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
     int len_x = 5;
@@ -14,6 +15,36 @@ int main() {
         printf("\n");
     }
     printf("\n");
+
+    int len_x2 = 5;
+    int len_y2 = 3;
+
+    int **arr2 = malloc(len_x2 * sizeof(int *));
+    if (arr2 == NULL) {
+        perror("Memory allocation failed");
+        return 1;
+    }
+
+    for (int i = 0; i < len_x2; i++) {
+        arr2[i] = malloc(len_y2 * sizeof(int));
+        if (arr2[i] == NULL) {
+            perror("Memory allocation failed");
+            return 1;
+        }
+    }
+
+    for (int i = 0; i < len_x2; i++) {
+        for (int j = 0; j < len_y2; j++) {
+            arr2[i][j] = (i * len_y2) + j + 1;
+            printf("%d ", arr2[i][j]);
+        }
+        printf("\n");
+    }
+
+    for (int i = 0; i < len_x2; i++) {
+        free(arr2[i]);
+    }
+    free(arr2);
 
     return 0;
 }
