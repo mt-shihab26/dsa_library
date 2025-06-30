a1d4f668401f830e397b58a21f91a2a2a464e09f
Shihab Mahamud <shihab4t@gmail.com>
2025-06-30 13:40:50 +0600

    loops

--- 01_c-fundamentals/003_loops.c
+++ 01_c-fundamentals/003_loops.c
@@ -6,7 +6,25 @@ int main() {
     while (i <= 10) {
         printf("index: %d\n", i);
         i++;
+        if (i == 5) {
+            continue;
+        }
     }
+    printf("\n");
+
+    for (int i = 0; i < 10; i++) {
+        if (i == 5) {
+            break;
+        }
+        printf("index: %d\n", i);
+    }
+    printf("\n");
+
+    int j = 0;
+    do {
+        printf("index: %d\n", j);
+        j++;
+    } while (j < 10);
 
     return 0;
 }
