7f0d9760248ae4225b5deadf13e3ad15da59dcbd
Shihab Mahamud <shihab4t@gmail.com>
2025-08-17 23:50:58 +0600

    reverse doubly linked list

--- 05_liked-lists/31_doubly-linked-list.cpp
+++ 05_liked-lists/31_doubly-linked-list.cpp
@@ -118,6 +118,46 @@ class DoublyLinkedList {
         tail->next = nullptr;
     }
 
+    void deleteAtPosition(int index) {
+        if (head == nullptr) {
+            return;
+        }
+        auto i = head;
+        auto y = index;
+        while (i != nullptr && y > 0) {
+            i = i->next;
+            y--;
+        }
+        if (y != 0) {
+            return;
+        }
+        // 10 <-> 25 <-> 30
+        auto prev = i->prev;
+        auto next = i->next;
+
+        if (prev == nullptr) {
+            head = next;
+        } else {
+            prev->next = next;
+        }
+        if (next == nullptr) {
+            tail = prev;
+        } else {
+            next->prev = prev;
+        }
+
+        delete i;
+    }
+
+    void reverse() {
+        auto i = head;
+        while (i != nullptr) {
+            cout << i->data << " ";
+            i = i->next;
+        }
+        cout << "\n";
+    }
+
     void displayForward() {
         auto i = head;
         while (i != nullptr) {
@@ -149,31 +189,36 @@ int main() {
     list->insertAtHead(10); // Insert 10 at the head: 10
     list->insertAtHead(20); // Insert 20 at the head: 20 <-> 10
 
-    // list->displayForward();  // Display forward: 20 <-> 10
-    // list->displayBackward(); // Display backward: 10 <-> 20
+    // list->displayForward();
+    // list->displayBackward();
 
     list->insertAtTail(30); // Insert 30 at the tail: 20 <-> 10 <-> 30
     list->insertAtTail(40); // Insert 40 at the tail: 20 <-> 10 <-> 30 <-> 40
 
-    // list->displayForward();  // Display forward: 20 <-> 10 <-> 30 <-> 40
-    // list->displayBackward(); // Display backward: 40 <-> 30 <-> 10 <-> 20
+    // list->displayForward();
+    // list->displayBackward();
 
     list->insertAtPosition(2, 25); // Insert 25 at position 2: 20 <-> 10 <-> 25 <-> 30 <-> 40
 
-    list->displayForward();  // Display forward: 20 <-> 10 <-> 25 <-> 30 <-> 40
-    list->displayBackward(); // Display backward: 40 <-> 30 <-> 25 <-> 10 <-> 20
+    // list->displayForward();
+    // list->displayBackward()
 
-    list->deleteAtHead(); // Delete the head: 10 <-> 25 <-> 30 <-> 40
-    list->deleteAtTail(); // Delete the tail: 10 <-> 25 <-> 30
-    // list->deleteAtPosition(1); // Delete at position 1: 10 <-> 30
-
-    list->displayForward();  // Display forward: 10 <-> 30
-    list->displayBackward(); // Display backward: 30 <-> 10
-
-    // $list->reverse(); // Reverse the list: 30 <-> 10
+    // list->deleteAtHead(); // Delete the head: 10 <-> 25 <-> 30 <-> 40
+    // list->displayForward();
     //
-    // $list->displayForward(); // Display forward after reverse: 30 <-> 10
+    // list->deleteAtTail(); // Delete the tail: 10 <-> 25 <-> 30
+    // list->displayForward();
     //
+    // list->deleteAtPosition(2); // Delete at position 1: 10 <-> 30
+    // list->displayForward();
+    //
+    // list->deleteAtPosition(0); // Delete at position 1: 30
+    // list->displayForward();
+
+    list->displayForward();
+    list->reverse();
+    list->displayForward();
+
     // $list->clear(); // Clear the list
 
     delete list; // Free the memory
